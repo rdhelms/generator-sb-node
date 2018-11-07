8759968b222799b37cb64e84826b376018797e2e
Robert Helms <robert.helms@smashingboxes.com>
2018-11-07 15:17:53 -0500

    Updated templates, updated deps, and upped version

--- generators/app/templates/src/app.ts
+++ generators/app/templates/src/app.ts
@@ -2,17 +2,22 @@ import express, { RequestHandler } from 'express';
 import session from 'express-session';
 import ConnectSessionSequelize from 'connect-session-sequelize';
 import { sequelize } from './sequelize/db';
+import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
 import passport from 'passport';
 import { localStrategy } from './passports/local.passport';
-import { router as indexController } from './controllers/index.controller';
-import { router as loginController } from './controllers/login.controller';
-import { router as signupController } from './controllers/signup.controller';
-import { TUser } from './types/User';
 import User from './sequelize/models/user.model';
+import { indexController } from './controllers/index.controller';
+import { usersController } from './controllers/users.controller';
+import { sessionsController } from './controllers/sessions.controller';
 
 const app = express();
+
+// CORS middleware
+app.use(cors({
+    origin: process.env.NODE_ENV !== 'production' ? true : false // reflects request origin if not production
+}));
 
 // Cookie middleware
 app.use(cookieParser());
@@ -32,14 +37,19 @@ app.use(session({
 }));
 
 // Parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false,
+    limit: '50mb'
+}));
 // Parse application/json
-app.use(bodyParser.json());
+app.use(bodyParser.json({
+    limit: '50mb'
+}));
 
 // Authentication middleware
 app.use(passport.initialize());
 app.use(passport.session() as RequestHandler);
-passport.serializeUser((user: TUser, done) => {
+passport.serializeUser((user: User, done) => {
     done(null, user.id);
 });
 passport.deserializeUser(async (id, done) => {
@@ -52,9 +62,11 @@ passport.deserializeUser(async (id, done) => {
         if (user) {
             done(null, user);
         } else {
+            /* istanbul ignore next */
             throw new Error('No user found');
         }
     } catch (err) {
+        /* istanbul ignore next */
         done(err);
     }
 });
@@ -62,9 +74,10 @@ passport.use(localStrategy);
 
 // Routes and Controllers
 app.use('/', indexController);
-app.use('/login', loginController);
-app.use('/signup', signupController);
+app.use('/sessions', sessionsController);
+app.use('/users', usersController);
 
+/* istanbul ignore next */
 if (process.env.NODE_ENV !== 'test') {
     const port = process.env.PORT || 3000;
     app.listen(port, () => {
